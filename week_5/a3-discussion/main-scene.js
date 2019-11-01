bd45891326d548177761ea1c6a33c6eb9df100d7
luckiday <guoyunqi@gmail.com>
2019-11-01 14:55:39 -0700

    Init

--- week_5/a3-discussion/main-scene.js
+++ week_5/a3-discussion/main-scene.js
@@ -17,6 +17,10 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
                 torus2: new (Torus.prototype.make_flat_shaded_version())(15, 15),
 
                 // TODO:  Fill in shapes for sun and planet 1
+
+                sun_1: new (Subdivision_Sphere)(4),
+                sun_2: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2),
+                planet1: new (Subdivision_Sphere.prototype.make_flat_shaded_version())(2)
             };
             this.submit_shapes(context, shapes);
 
@@ -24,14 +28,26 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
             this.materials =
                 {
                     test: context.get_instance(Phong_Shader).material(Color.of(1, 1, 0, 1), {ambient: .2}),
-
                     // TODO:  Fill the the materials for sun and planet 1
+                    sun: context.get_instance(Phong_Shader).material(Color.of(1, .5, .5, 1), {
+                        ambient: 1,
+                        // diffusivity: .7,
+                        // specular: 1,
+                        // gouraud: true,
+                    }),
+                    planet1: context.get_instance(Phong_Shader).material(Color.of(.9, .9, .9, 1), {
+                        ambient: .5,
+                        diffusivity: .7,
+                        specular: 1,
+                        gouraud: true,
+                    })
                 };
 
-            this.lights = [new Light(Vec.of(5, 10, 10, 1), Color.of(1, 0, 0, 1), 1000)];
+            this.lights = [new Light(Vec.of(0, 0, 0, 1), Color.of(1, 0, 0, 1), 1000)];
             // TODO: Change the light position
 
             // TODO: Initialize attached function
+            this.attached = () => this.initial_camera_location;
         }
 
         make_control_panel()            // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
@@ -47,12 +63,39 @@ window.Assignment_Three_Scene = window.classes.Assignment_Three_Scene =
 
             // TODO:  Draw the sun
 
+            let transformation_sun = Mat4.identity();
+            transformation_sun = transformation_sun.times(Mat4.scale([2, 2, 2]));
+            this.shapes.sun_1.draw(graphics_state, transformation_sun, this.materials.sun);
+
             // TODO: Draw Planet 1
 
+            let transformation_planet = Mat4.identity();
+            transformation_planet =
+                transformation_planet.times(Mat4.rotation(2 * t, Vec.of(0, 1, 0)));
+            transformation_planet =
+                transformation_planet.times(Mat4.translation([5, 0, 0]));
+            transformation_planet =
+                transformation_planet.times(Mat4.rotation(-2 * t, Vec.of(0, 1, 0)));
+            transformation_planet =
+                transformation_planet.times(Mat4.rotation(t, Vec.of(1, 1, 0)));
+
+
+            this.shapes.planet1.draw(graphics_state, transformation_planet, this.materials.planet1);
+
             // TODO: Change to the planet view
+            this.planet_1 = transformation_planet;
+            let camera_matrix = this.attached();
+            console.log(camera_matrix);
+            if (camera_matrix === this.initial_camera_location){
+                graphics_state.camera_transform = Mat4.inverse(camera_matrix)
+                    .map( (x,i) => Vec.from( graphics_state.camera_transform[i] ).mix( x, .5 ) );;
+            } else {
+                let camera_planet_transformation =
+                    Mat4.translation([0,0,-5]).times(Mat4.inverse(camera_matrix));
+                graphics_state.camera_transform = camera_planet_transformation
+                    .map( (x,i) => Vec.from( graphics_state.camera_transform[i] ).mix( x, .5 ) );
+            }
 
-
-            this.shapes.torus2.draw(graphics_state, Mat4.identity(), this.materials.test);
 
         }
     };
