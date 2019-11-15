e9216f5c7601b629765913052468a74bf1dfda1e
luckiday <guoyunqi@gmail.com>
2019-11-15 14:09:15 -0800

    Week 7 discussion

--- week_7/demo/main-scene.js
+++ week_7/demo/main-scene.js
@@ -18,7 +18,8 @@ window.Assignment_Four_Scene = window.classes.Assignment_Four_Scene =
                 box_1: new Cube(),
                 axis: new Axis_Arrows()
             };
-            
+            shapes.box_1.texture_coords = shapes.box_1.texture_coords.map(v => Vec.of(v[0] * 2, v[1] * 3));
+
             this.submit_shapes(context, shapes);
 
             // TODO:  Create the materials required to texture both cubes with the correct images and settings.
@@ -27,7 +28,12 @@ window.Assignment_Four_Scene = window.classes.Assignment_Four_Scene =
             this.materials =
                 {
                     phong: context.get_instance(Phong_Shader).material(Color.of(1, 1, 0, 1)),
-                    box_1: context.get_instance(Phong_Shader).material(Color.of(1, 1, 0, 1)),
+                    box_1: context.get_instance(Texture_Rotate).material(
+                        Color.of(0, 0, 0, 1), {
+                            ambient: 1,
+                            texture: context.get_instance("assets/demo_2.png")
+                        }
+                    ),
                 };
 
             this.lights = [new Light(Vec.of(-5, 5, 5, 1), Color.of(0, 1, 1, 1), 100000)];
@@ -45,7 +51,8 @@ window.Assignment_Four_Scene = window.classes.Assignment_Four_Scene =
             const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
 
             // TODO:  Draw the required boxes. Also update their stored matrices.
-            this.shapes.axis.draw(graphics_state, Mat4.identity(), this.materials.phong);
+            // this.shapes.axis.draw(graphics_state, Mat4.identity(), this.materials.phong);
+            this.shapes.box_1.draw(graphics_state, Mat4.identity(), this.materials.box_1);
         }
     };
 
@@ -62,7 +69,8 @@ class Texture_Rotate extends Phong_Shader {
             return;
           } 
           // Do smooth "Phong" shading unless options like "Gouraud mode" are wanted instead.      
-          // Otherwise, we already have final colors to smear (interpolate) across vertices.                                      // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
+          // Otherwise, we already have final colors to smear (interpolate) across vertices.                                      
+          // If we get this far, calculate Smooth "Phong" Shading as opposed to Gouraud Shading.
           // Phong shading is not to be confused with the Phong Reflection Model.               
           // Sample the texture image in the correct place.
           // Compute an initial (ambient) color:
@@ -70,8 +78,11 @@ class Texture_Rotate extends Phong_Shader {
           vec4 tex_color = texture2D( texture, f_tex_coord );        
                     
           /* 2D rotation matrix */
-
-                                                                                    
+          // float theta = animation_time;
+          // mat2 r = mat2( cos(theta), sin(theta), -sin(theta), cos(theta));
+          // float t = 0.5;
+          // vec4 tex_color = texture2D( texture, r * (f_tex_coord.xy - t) + t);
+                                                                            
           if( USE_TEXTURE ) 
             gl_FragColor = vec4( ( tex_color.xyz + shapeColor.xyz ) * ambient, 
               shapeColor.w * tex_color.w ); 
